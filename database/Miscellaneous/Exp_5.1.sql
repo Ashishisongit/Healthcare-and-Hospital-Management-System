59e92775f6be87d60d0657a729b638649e853cfe
Ashish Here ! <alphaxrajput@gmail.com>
2026-09-22 10:16:21 +0530

    Add salary update query for hospital staff
    Added a query to update salaries for specific staff members.

--- database/Miscellaneous/Exp_5.1.sql
+++ database/Miscellaneous/Exp_5.1.sql
@@ -5,3 +5,12 @@ select Bed_no from bed where ward_id=4;
 select First_Name,Last_Name,Blood_Group from patient order by DOB DESC;
 Select * From hospital_Staff where First_name Like 'A%'; -- Starts with A Condition , Logical Operation
 select Contact from hospital_Staff where Gender='Female';
+Update hospital_staff
+SET Salary = CASE Staff_ID
+    WHEN 1 THEN 124578
+    WHEN 2 THEN 154556
+    WHEN 3 THEN 245689
+    WHEN 4 THEN 214589
+    WHEN 5 THEN 325645
+END
+WHERE Staff_ID IN (1, 2, 3, 4, 5);
